c4a7e2cc5366cffb891568a1e684d34ff38d3ea6
puccia <puccia@gmail.com>
2011-01-21 13:02:27 +0000

    Use NullBooleanField where appropriate

--- django_cmdbuild/models/__init__.py
+++ django_cmdbuild/models/__init__.py
@@ -38,7 +38,7 @@ class Lookup(models.Model):
       number = models.IntegerField(help_text=u'Topic', blank=True, null=True, db_column='Number')
       code = models.CharField(help_text=u'Topic', max_length=100, blank=True, db_column='Code')
       description = models.CharField(help_text=u'Topic', max_length=100, blank=True, db_column='Description')
-      isdefault = models.BooleanField(help_text=u'Topic', blank=True, null=True, db_column='IsDefault')
+      isdefault = models.NullBooleanField(help_text=u'Topic', blank=True, null=True, db_column='IsDefault')
       status = models.TextField(help_text=u'Topic', blank=True, db_column='Status') # This field type is a guess.
       notes = models.TextField(help_text=u'Topic', blank=True, db_column='Notes')
       class Admin:
